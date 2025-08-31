fd9f81d58120c53ece79e4bd02e3c36ff78caaad
ehtisham432 <46423670+ehtisham432@users.noreply.github.com>
2025-08-31 15:14:07 +0500

    Test First stage

---
+++ springwork/inventory-management/src/main/java/com/example/inventory/Product.java
@@ -0,0 +1,61 @@
+package com.example.inventory;
+
+import jakarta.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    @OneToMany(mappedBy = "product")
+    @com.fasterxml.jackson.annotation.JsonIgnore
+    private Set<Inventory> inventories;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Set<Inventory> getInventories() {
+        return inventories;
+    }
+
+    public void setInventories(Set<Inventory> inventories) {
+        this.inventories = inventories;
+    }
+}
